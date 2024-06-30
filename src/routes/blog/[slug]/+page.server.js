079b248ab028e41ba4f91637b0200333fdc04832
Rohan Sharan <rohansharan@protonmail.com>
2024-06-30 16:07:21 +0530

    initial

---
+++ src/routes/blog/[slug]/+page.server.js
@@ -0,0 +1,6 @@
+export async function load({params}) {
+    return {
+       content: `hello ${params.slug}`
+    }
+
+}
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+export let data
+
+</script>
+
+<p> {data.content}</p>
+

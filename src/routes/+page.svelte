cce2db1b92cc537dc47530eefa0101aa9f43faca
Rohan Sharan <rohansharan@protonmail.com>
2024-06-30 16:45:42 +0530

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="ml-20 pt-20 pl-20 hover:underline font-bold hover:text-red-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
